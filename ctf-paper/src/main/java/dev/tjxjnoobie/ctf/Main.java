c319d55667a10ae1ed0832cc38db136dd8d84e01
TJ <tjxjnoobie@gmail.com>
2026-02-28 04:30:03 -0800

    Added: Create 'Main' as the initial Paper plugin entrypoint so the plugin has a real bootstrap skeleton.

---
+++ ctf-paper/src/main/java/dev/tjxjnoobie/ctf/Main.java
@@ -0,0 +1,12 @@
+﻿package dev.tjxjnoobie.ctf;
+
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
