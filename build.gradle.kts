01afee81ac99abe0b518c13abe4717100ca0d8f8
TJ <tjxjnoobie@gmail.com>
2026-02-28 04:30:05 -0800

    Added: Create 'settings.gradle.kts' and 'build.gradle.kts' so the project can build and run through Gradle.

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+﻿plugins {
+    base
+}
+
+allprojects {
+    group = "com.minecraftctf"
+    version = "0.1.0-SNAPSHOT"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "Minecraft-CTF"
+include("paper")
+project(":paper").projectDir = file("ctf-paper")
