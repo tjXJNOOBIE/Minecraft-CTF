175b215efb5d7887d34a1bdfa5f1d7c60d07fded
TJ <tjxjnoobie@gmail.com>
2026-03-01 12:22:53 -0800

    Added: Create 'KitType' to define kit choices in code.

---
+++ ctf-paper/src/main/java/dev/tjxjnoobie/ctf/kit/tags/KitType.java
@@ -0,0 +1,12 @@
+package dev.tjxjnoobie.ctf.kit.tags;
+
+/**
+ * Represents available CTF kits.
+ */
+public enum KitType {
+    // Fast mobility kit.
+    SCOUT,
+    // Standard ranged kit.
+    RANGER
+}
+
