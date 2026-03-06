fd2e3817f1a11504bf2d2317cb3d941722f12b43
TJ <tjxjnoobie@gmail.com>
2026-03-06 11:12:02 -0800

    Added: Create 'IPlayerTeleportEventHandler' and 'IFlagBreakEventHandler' so the related runtime flow is handled through dedicated listeners.

---
+++ ctf-paper/src/main/java/dev/tjxjnoobie/ctf/events/handlers/IFlagBreakEventHandler.java
@@ -0,0 +1,7 @@
+package dev.tjxjnoobie.ctf.events.handlers;
+
+import org.bukkit.event.block.BlockBreakEvent;
+
+public interface IFlagBreakEventHandler {
+    void onBlockBreak(BlockBreakEvent event);
+}
---
+++ ctf-paper/src/main/java/dev/tjxjnoobie/ctf/events/handlers/IPlayerTeleportEventHandler.java
@@ -0,0 +1,7 @@
+package dev.tjxjnoobie.ctf.events.handlers;
+
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public interface IPlayerTeleportEventHandler {
+    void onPlayerTeleport(PlayerTeleportEvent event);
+}
